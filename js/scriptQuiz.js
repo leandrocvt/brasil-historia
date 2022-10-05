47b2de3708801341e9bf0e6d8ca0d85e54be5ca6
Vitoria Cristinni <vitoria.ctistinni@gmail.com>
2022-10-05 13:25:21 -0300

    Adicionando os textos e imagens

--- js/scriptQuiz.js
+++ js/scriptQuiz.js
@@ -5,46 +5,112 @@ $('#quiz').quiz({
   counterFormat: 'Question %current of %total',
   questions: [
     {
-      'q': 'Bacon ipsum dolor amet porchetta kevin filet mignon?',
+      'q': 'O que foi o período Pré-Cabralino?',
       'options': [
-        'Yes',
-        'No'
+        'O momento para a história do Brasil antes da chegada do navegador português Pedro Álvares Cabral, em 1500.',
+        'O momento para a história do Brasil logo após a partida do navegador português Pedro Álvares Cabral, em 1520.'
       ],
       'correctIndex': 0,
-      'correctResponse': 'Correct! Spare ribs salami sirloin short ribs capicola.',
-      'incorrectResponse': 'Incorrect! Spicy jalapeno venison cupim shankle picanha.'
+      'correctResponse': 'Correto! Estou feliz que você entendeu a matéria',
+      'incorrectResponse': 'Errado, mas não desanime, volte na máteria ou veja um dos nossos vídeos recomendados para entender melhor o assunto'
     },
     {
-      'q': 'Bacon ham tenderloin pork belly pork chop bresaola pancetta?',
+      'q': 'O período Pré-Colonial se encerrou com que implantação?',
       'options': [
-        'Steak',
-        'Ribs',
-        'Sirloin'
+        'Com a implantação do sistemas operacionais.',
+        'Com a implantação do sistema de capitalista hereditárias',
+        'Com a implantação do sistema imperial'
       ],
-      'correctIndex': 2,
-      'correctResponse': 'Correct! Bresaola venison picanha, brisket t-bone tenderloin salami jowl chicken landjaeger.',
-      'incorrectResponse': 'Incorrect! Kevin bresaola meatball ball tip ground round.'
+      'correctIndex': 1,
+      'correctResponse': 'Correto! Você está indo muito bem',
+      'incorrectResponse': 'Errado, entendo que esse assunto é um pouco mais difícil. O que acha de reler a matéria e tentar novamente?'
     },
     {
-      'q': 'Corned beef hamburger alcatra biltong, beef ribs flank brisket?',
+      'q': 'No Período Colonial (1494) o Reino de Portugal e a Coroa da Inglaterra assinaram um acordo que junta territorialmente as terras descobertas na América, acordo conhecido como Tratado de Tordesilhas',
       'options': [
-        'True',
-        'False'
+        'Verdadeiro',
+        'Falso'
       ],
       'correctIndex': 0,
-      'correctResponse': 'Correct! Bacon meatloaf beef porchetta jerky t-bone pastrami.',
-      'incorrectResponse': 'Incorrect! Pork loin fatback shankle.'
+      'correctResponse': 'Correto! Muito bom',
+      'incorrectResponse': 'Errado, que tal conferirmos a time-line do período colonial?'
     },
     {
-      'q': 'Picanha meatball tenderloin burgdoggen cow jowl?',
+      'q': 'Quando teve ínicio o período imperial?',
       'options': [
-        'Sausage',
-        'Tenderloin',
-        'Picanha'
+        '1863',
+        '1850',
+        '1822'
       ],
       'correctIndex': 2,
-      'correctResponse': 'Correct! Jowl frankfurter burgdoggen turkey, porchetta strip steak landjaeger jerky.',
-      'incorrectResponse': 'Incorrect! Swine andouille ground round, short ribs short loin.'
+      'correctResponse': 'Correto! Parabéns',
+      'incorrectResponse': 'Errado, vamos tentar outra vez?'
+    },
+    {
+      'q': 'No primeiro reinado, quem governou o Brasil?',
+      'options': [
+        'Pedro |',
+        'Pedro ||',
+        'Pedro |V'
+      ],
+      'correctIndex': 0,
+      'correctResponse': 'Correto! Muito bom',
+      'incorrectResponse': 'Errado, mas relaxa, vamos ver um vídeo sobre o assunto?'
+    }, 
+    {
+      'q': 'Por que Dom Pedro || não podia assumir o trono no período regerencial?',
+      'options': [
+        'Deveria aguardar pois estava sendo julgado por um crime',
+        'O povo não queria ele no trono',
+        'Deveria aguardar a sua maioridade'
+      ],
+      'correctIndex': 2,
+      'correctResponse': 'Correto! Muito bom ver sua evolução',
+      'incorrectResponse': 'Errado, na próxima você consegue!'
+    },
+    {
+      'q': 'O que marcou o segundo reinado?',
+      'options': [
+        'Inúmeras transfrormaçôes',
+        'Inúmeras destruições',
+        'Inúmeras conquistas'
+      ],
+      'correctIndex': 0,
+      'correctResponse': 'Correto! Foguete não tem ré',
+      'incorrectResponse': 'Errado, pode ser uma boa idéia reler sobre o assunto'
+    },
+    {
+      'q': 'Durante o período republicano ocorreu um golpe feito por Marechal Deodoro da Fonseca, quando ocorreu esse golpe?',
+      'options': [
+        '18 de novembro de 1789',
+        '12 de novembro de 1839',
+        '15 de novembro de 1889'
+      ],
+      'correctIndex': 2,
+      'correctResponse': 'Correto! Temos um gênio entre nós?',
+      'incorrectResponse': 'Errado, mas não se preocupe, temos alguns vídeos bem bacanas que podem te ajudar a fixar a matéria.'
+    },
+    {
+      'q': 'A primeira república é muito conhecida, não somente por "primeira república" mas também por:',
+      'options': [
+        'República Velha ou República das Oligarquias',
+        'República Nova ou Sistemas das Oligarquias',
+        'República Antiga ou República das Oligarquias'
+      ],
+      'correctIndex': 0,
+      'correctResponse': 'Correto! Você vai se dar bem na prova.',
+      'incorrectResponse': 'Errado, mas que tal se aprofunda mais no assunto lendo um dos e-books recomendados?'
+    },
+    {
+      'q': 'Quando foi finalizado o período Era Vargas?:',
+      'options': [
+        '1955',
+        '1985',
+        '1945'
+      ],
+      'correctIndex': 2,
+      'correctResponse': 'Correto! Você é um sucesso',
+      'incorrectResponse': 'Errado, mas não desanime, volte na máteria ou veja um dos nossos vídeos recomendados para entender melhor o assunto.'
     }
   ]
 });
